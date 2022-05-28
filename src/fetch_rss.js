c058a5e558a49edab4615c8776eed09d2e52e4bf
cacao math <mahiro.1192@gmail.com>
2022-05-28 14:35:28 +0900

    日付が指定した日より後のものを抽出するように処理の追加

--- src/fetch_rss.js
+++ src/fetch_rss.js
@@ -6,13 +6,17 @@ export async function fetch_rss(URL){
 
     let feed = await parser.parseURL(URL);
     console.log(feed.title);
+
+    let result = [];
   
     feed.items.forEach(item => {
-      if(item["dc:date"]===undefined){
-        console.log(item.title, item.link, item["pubDate"]);
-      }else{
-        console.log(item.title, item.link, item["dc:date"]);
+      if(item["dc:Date"] !== undefined){
+        item.pubdate = item["dc:Date"];
+      }
+      if(new Date('Thu, 23 May 2022 16:20:00 +0900') < new Date(item.pubDate)){
+        result.push({title: item.title, link: item.link, date: item.pubDate});
       }
     });
-    console.log("--------");
+    console.log(result);
+    return result;
   };
