bd1ec1daa35a8f52dc2c62f3bf8cdea3e886f4f0
cacao math <mahiro.1192@gmail.com>
2022-05-29 09:37:48 +0900

    コード整理

--- src/fetch_rss.js
+++ src/fetch_rss.js
@@ -6,8 +6,11 @@ export async function fetch_rss(baseDate, URL, pattern){
     const feed = await parser.parseURL(URL);
     const result = feed.items
     .map(item =>{
-      return ({title: item.title, link: item.link, pubDate: (item["dc:Date"] !== undefined) ? item["dc:Date"] : item["pubDate"]})
+      return ({
+        title: item.title,
+        link: item.link,
+        pubDate: (item["dc:Date"] !== undefined) ? new Date(item["dc:Date"]) : new Date(item["pubDate"])})
      })
-    .filter(item => baseDate < new Date(item.pubDate) && pattern.test(item.title));
+    .filter(item => baseDate < item.pubDate && pattern.test(item.title));
     return result;
   };
