b08f74f8378905d082189628ea39e92b7791dd6a
cacao math <mahiro.1192@gmail.com>
2022-09-19 01:11:13 +0900

    feat: 文字解析にMecabを追加, tiny-segmenterを削除

--- src/estimation.js
+++ src/estimation.js
@@ -1,10 +1,20 @@
 import bayes from 'bayes';
 import fs from 'fs';
-import TinySegmenter from 'tiny-segmenter';
+import Mecab from 'mecab-async';
 
-const segmenter = new TinySegmenter()
+async function my_mecab(text){
+  return await new Promise((resolve,reject) => {
+    Mecab.parse(text, function(err, result) {
+      if (err) reject(err)
+  
+      const wakachi = result.filter(x => /^動詞|名詞|形容詞/.test(x)).map(x => x[0])
+      resolve(wakachi.filter(v => v))
+    })
+  })
+}
+
 const classifier = bayes({
-  tokenizer: function (text) { return segmenter.segment(text) } 
+  tokenizer: async function (text) { return await my_mecab(text) } 
 });
 
 function file_importer(text_path){
@@ -18,11 +28,10 @@ async function learn_classifier(classifier, txt, category){
 }
 const texts1 = file_importer('./resource/grand_true.txt');
 for(const txt of texts1){
-  console.log(segmenter.segment(txt))
   await learn_classifier(classifier, txt, "windows");
 }
 const texts2 = file_importer('./resource/grand_false.txt');
 for(const txt of texts2){
-  await learn_classifier(classifier, txt, "non windows");
+  await learn_classifier(classifier, txt, "other");
 }
-await console.log(await classifier.categorize('ハピネス'));
+console.log(await classifier.categorize('しゃけ'));
